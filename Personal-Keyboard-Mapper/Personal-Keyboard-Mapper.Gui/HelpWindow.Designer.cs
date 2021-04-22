fef7a42d79429348187d1485036d05c38769c8db
Leszek Lorens <krulon@o2.pl>
2021-04-22 16:49:02 +0200

    gui project added

---
+++ Personal-Keyboard-Mapper/Personal-Keyboard-Mapper.Gui/HelpWindow.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+using System.Windows.Forms;
+
+namespace Personal_Keyboard_Mapper.Gui
+{
+    partial class HelpWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.possibleKeysTable = new System.Windows.Forms.DataGridView();
+            this.Column0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rows = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.possibleKeysTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // possibleKeysTable
+            // 
+            this.possibleKeysTable.AllowUserToAddRows = false;
+            this.possibleKeysTable.AllowUserToDeleteRows = false;
+            this.possibleKeysTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.possibleKeysTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.possibleKeysTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.possibleKeysTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column0,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.possibleKeysTable.Location = new System.Drawing.Point(0, 0);
+            this.possibleKeysTable.Name = "possibleKeysTable";
+            this.possibleKeysTable.ReadOnly = true;
+            this.possibleKeysTable.RowHeadersWidth = 51;
+            this.possibleKeysTable.RowTemplate.Height = 24;
+            this.possibleKeysTable.Size = new System.Drawing.Size(1035, 79);
+            this.possibleKeysTable.TabIndex = 0;
+            // 
+            // Column0
+            // 
+            this.Column0.HeaderText = "0";
+            this.Column0.MinimumWidth = 6;
+            this.Column0.Name = "Column0";
+            this.Column0.ReadOnly = true;
+            this.Column0.Width = 45;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 45;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 45;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 45;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 45;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "5";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 45;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "6";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 45;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "7";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 45;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "8";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 45;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "9";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 45;
+            // 
+            // Rows
+            // 
+            this.Rows.MinimumWidth = 6;
+            this.Rows.Name = "Rows";
+            this.Rows.ReadOnly = true;
+            this.Rows.Width = 71;
+            // 
+            // HelpWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 61);
+            this.Controls.Add(this.possibleKeysTable);
+            this.Name = "HelpWindow";
+            this.Text = "Help window";
+            ((System.ComponentModel.ISupportInitialize)(this.possibleKeysTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView possibleKeysTable; 
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private DataGridViewTextBoxColumn Rows;
+    }
+}
---
+++ Personal-Keyboard-Mapper/Personal-Keyboard-Mapper.Gui/HelpWindow.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+
+namespace Personal_Keyboard_Mapper.Gui
+{
+    public partial class HelpWindow : Form
+    {
+        private ILog logger;
+        public HelpWindow(ILog log)
+        {
+            InitializeComponent();
+            logger = log;
+        }
+
+        public void FillHelperRow(IEnumerable<string> possibleKeyActions)
+        {
+            if (possibleKeyActions == null)
+            {
+                logger.Error("Could not fill an help window, no data");   
+            }
+
+            if (possibleKeysTable.Rows.Count == 0)
+            {
+                possibleKeysTable.Rows.Add(new DataGridViewRow());
+            }
+            var cellIndex = 0;
+            foreach (var action in possibleKeyActions)
+            {
+                possibleKeysTable.Rows[0].Cells[cellIndex].Value = possibleKeyActions.ToArray()[cellIndex++];
+            }
+        }
+
+        public void ClearHelperRow()
+        {
+            if (possibleKeysTable.Rows.Count == 1)
+            {
+                foreach (DataGridViewCell cell in possibleKeysTable.Rows[0].Cells)
+                {
+                    cell.Value = "";
+                }
+            }
+        }
+
+    }
+}
